531901738bdee4f4a1bb44c79974a0586f6545bf
Shivam Kumar <47521739+shkm34@users.noreply.github.com>
2025-11-06 21:09:54 +0530

    Update CORS origin to use environment variable

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
 }));
